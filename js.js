6dec56aa1b34a4bd56b69c89ae5b04fc474db4a6
Home <home@Homes-MacBook-Pro.local>
2017-04-25 15:47:57 +0200

    Dziala licznik i zatrzymuje gre :D

--- js.js
+++ js.js
@@ -1,8 +1,30 @@
 
-  window.onload = setInterval(start,500);
   
 
+  
+$(document).ready(function(){
 
+  var startInterval = setInterval(start,500);
+  
+
+      var count = 60; // ustawiłem licznik 60 sekund gry tak jak miało być
+      countDown();
+      var counter = setInterval(countDown, 1000); // co 1 sekunde w teorii
+      
+
+      function countDown(){
+          var out = count;
+          if(count<10) out = '0' + count; // koncówka mniej niż 10 sekund to wyświetli sekundy w formacie 00 zamiast 0
+          console.log(out);
+          $("#timer").html(out); // przypisanie tekstu timera do odpowiedniego elementu html
+          if( count <= 0){
+              //licznik się wyzerował należy podjąć odpowiednią akcje
+              clearInterval(counter); //zatrzymanie licznika i wraca
+              clearInterval(startInterval); //zatrzymanie licznika i wraca
+              return;
+          }
+          --count;
+      }
 
   function start(){
 
@@ -27,16 +49,11 @@
   var losoweCoiny = Math.round(Math.random()*2);
 
 
-
-  
-
-
   nowyDiv.className = 'obiekt'+losoweCoiny;
 
   // nowyDiv.className = className;
   document.body.appendChild(nowyDiv);
   // var klocek = document.querySelector('.'+className);
-  console.log(losoweCoiny);
 
   nowyDiv.style.top=y+"px";
   nowyDiv.style.left=x+"px";
@@ -51,16 +68,20 @@
 
 //koniec poprawek
 
-
+  
 
   
-//grawitacja obiektu po zniknie z ekranu automatycznie sie usuwa
-  setInterval (function spadaj(){
-    if(y<(wysokoscKlienta)){
-    y++;
-    nowyDiv.style.top=y+'px';
-  } else{nowyDiv.remove();}
-},10)
-}
+  //grawitacja obiektu po zniknie z ekranu automatycznie sie usuwa
+  var spadajInterval = setInterval (function spadaj(){
+        if(y<(wysokoscKlienta)){
+        y++;
+        nowyDiv.style.top=y+'px';
+      } else{nowyDiv.remove();}
+    },10)
+  }
+
+});
+
+ 
 
 
